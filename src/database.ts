4c7978a22829355b15c849700144a8a0b73d3768
Gabriel Oliveira <gogabriel35@gmail.com>
2026-04-16 20:25:42 -0300

    fix database ipv4

--- src/database.ts
+++ src/database.ts
@@ -1,23 +1,21 @@
-import 'dotenv/config'
-import { knex as setupKnex, Knex } from 'knex'
-import { env } from './env'
-import { attachPaginate } from 'knex-paginate'
+import "dotenv/config";
+import { knex as setupKnex, Knex } from "knex";
+import { env } from "./env";
+import { attachPaginate } from "knex-paginate";
 
 export const config: Knex.Config = {
-    client: 'pg',
-    connection: {
-        host: env.DATABASE_HOST,
-        port: env.DATABASE_PORT,
-        user: env.DATABASE_USER,
-        password: env.DATABASE_PASSWORD,
-        database: env.DATABASE_NAME,
-        options: `--client_encoding=UTF8`,
-        family: 4,
-    },
-    migrations: {
-        extension: 'ts',
-        directory : './src/database/migrations'
-    }
-}
-export const knex = setupKnex(config)
+  client: "pg",
+  connection: {
+    host: env.DATABASE_HOST,
+    port: env.DATABASE_PORT,
+    user: env.DATABASE_USER,
+    password: env.DATABASE_PASSWORD,
+    database: env.DATABASE_NAME,
+  },
+  migrations: {
+    extension: "ts",
+    directory: "./src/database/migrations",
+  },
+};
+export const knex = setupKnex(config);
 attachPaginate();
